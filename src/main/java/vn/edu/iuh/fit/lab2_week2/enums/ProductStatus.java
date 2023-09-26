3c248b2380278ed6a14200e50eb0cabbcd025d79
MinhDuy18 <duy19102018@gmail.com>
2023-09-27 00:54:42 +0700

    Create Enums

--- src/main/java/vn/edu/iuh/fit/lab2_week2/enums/ProductStatus.java
+++ src/main/java/vn/edu/iuh/fit/lab2_week2/enums/ProductStatus.java
@@ -1,4 +1,16 @@
 package vn.edu.iuh.fit.lab2_week2.enums;
 
 public enum ProductStatus {
+    ACTIVE(1),
+    IN_ACTIVE(0),
+    TERMINATED(-1);
+
+    private int value;
+
+    ProductStatus(int value) {
+        this.value = value;
+    }
+    public int getValue(){
+        return value;
+    }
 }
